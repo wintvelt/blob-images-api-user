fca33303995a463bd6e64e1d92949d82e4529d48
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-06 17:15:46 +0200

    add user pool env

--- serverlessDomainConfig.js
+++ serverlessDomainConfig.js
@@ -7,3 +7,8 @@ module.exports.photoTable = () => ({
     'dev': 'blob-images-photos-dev',
     'prod': 'blob-images-photos'
 });
+
+module.exports.userPoolId = () => ({
+    'dev': 'eu-central-1_3N6XUNPAn',
+    'prod': 'eu-central-1_yZTQaYgj6'
+});
