1679f851cd05c0812b3d93692de785627bbdda98
Wouter <wintvelt@me.com>
2022-10-17 16:59:59 +0200

    skip create user for existing users + npm updates

--- handlersUser/createUser.js
+++ handlersUser/createUser.js
@@ -1,6 +1,8 @@
 // called by AWS cognito post confirmation trigger
+// NB: this happens at signup, but also after forgotpassword flow
 import { sanitize } from 'blob-common/core/sanitize';
 import { dbCreateItem } from "blob-common/core/dbCreate";
+import { dynamoDb } from 'blob-common/core/db';
 
 const NUMPICS = 22;
 const randomKnor = () => {
@@ -15,28 +17,38 @@ export const main = async (event, context, callback) => {
     const userSub = userAttributes?.sub;
 
     if (userSub) {
-        const name = userAttributes['custom:name'];
-        const email = userAttributes.email;
-        const inviteId = userAttributes['custom:inviteId'];
-
-        const UBItem = {
+        // check if user already exists
+        const UBkey = {
             PK: 'UBbase',
             SK: 'U' + userSub,
-            name: sanitize(name),
-            email: email.toLowerCase(),
-            photoUrl: randomKnor()
         };
-        const UPItem = {
-            PK: 'UPstats',
-            SK: 'U' + userSub,
-            photoCount: 0,
-            inviteId
-        };
+        const userResult = await dynamoDb.get({ Key: UBkey });
+        const existingUser = userResult.Item;
 
-        await Promise.all([
-            dbCreateItem(UBItem),
-            dbCreateItem(UPItem),
-        ]);
+        // only create user if there was none
+        if (!existingUser) {
+            const name = userAttributes['custom:name'];
+            const email = userAttributes.email;
+            const inviteId = userAttributes['custom:inviteId'];
+
+            const UBItem = {
+                ...UBkey,
+                name: sanitize(name),
+                email: email.toLowerCase(),
+                photoUrl: randomKnor()
+            };
+            const UPItem = {
+                PK: 'UPstats',
+                SK: 'U' + userSub,
+                photoCount: 0,
+                inviteId
+            };
+
+            await Promise.all([
+                dbCreateItem(UBItem),
+                dbCreateItem(UPItem),
+            ]);
+        }
     }
     // return the event to cognito
     callback(null, event);
